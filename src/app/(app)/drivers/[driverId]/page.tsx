fe923064f15f3c5bc7d2c3d252a52f5f584834e4
Lowe Löwing <lowe.lowing@gmail.com>
2025-02-27 01:00:01 +0100

    feat: configure ESLint to ignore errors during builds and refactor DriverPage for improved parameter handling

--- src/app/(app)/drivers/[driverId]/page.tsx
+++ src/app/(app)/drivers/[driverId]/page.tsx
@@ -4,37 +4,30 @@ import { notFound } from "next/navigation";
 import { getDriverById } from "@/lib/api/drivers/queries";
 import OptimisticDriver from "./OptimisticDriver";
 
-
 import { BackButton } from "@/components/shared/BackButton";
 import Loading from "@/app/loading";
 
-
 export const revalidate = 0;
 
-export default async function DriverPage({
-  params,
-}: {
-  params: { driverId: string };
-}) {
+export default async function DriverPage({ params }: { params: Promise<{ driverId: string }> }) {
+  const { driverId } = await params;
 
   return (
     <main className="overflow-auto">
-      <Driver id={params.driverId} />
+      <Driver id={driverId} />
     </main>
   );
 }
 
 const Driver = async ({ id }: { id: string }) => {
-  
   const { driver } = await getDriverById(id);
-  
 
   if (!driver) notFound();
   return (
     <Suspense fallback={<Loading />}>
       <div className="relative">
         <BackButton currentResource="drivers" />
-        <OptimisticDriver driver={driver}  />
+        <OptimisticDriver driver={driver} />
       </div>
     </Suspense>
   );
