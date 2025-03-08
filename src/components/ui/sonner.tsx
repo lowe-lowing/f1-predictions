8634ee3d0641eb68095b782949adc2d3cc418c9b
Lowe Löwing <lowe.lowing@gmail.com>
2025-03-08 18:49:55 +0100

    feat: enhance toaster functionality to handle redirection with improved toast notifications

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
@@ -10,17 +11,20 @@ const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
 
   const router = useRouter();
+  const path = usePathname();
   const searchParams = useSearchParams();
-  const redirected = searchParams.get("redirected");
-  if (redirected !== null) {
-    const page = usePathname();
-    toast(`Redirected to ${page}`);
-    console.log(`Redirected to ${page}`);
-    const params = new URLSearchParams(searchParams);
-    params.delete("redirected");
-    const newUrl = `${page}?${params.toString()}`;
-    router.replace(newUrl);
-  }
+
+  useEffect(() => {
+    const redirected = searchParams.get("redirected");
+    if (redirected !== null) {
+      toast(`Redirected to ${path}`);
+      console.log(`Redirected to ${path}`);
+      const params = new URLSearchParams(searchParams);
+      params.delete("redirected");
+      const newUrl = `${path}?${params.toString()}`;
+      router.replace(newUrl);
+    }
+  }, []);
 
   return (
     <Sonner
