97bd5d2ae80c3fb4a70fbe430a1c93930ec404ef
Lowe Löwing <76205967+lowe-lowing@users.noreply.github.com>
2025-03-12 02:33:01 +0100

    Update DriverComponent.tsx

--- src/app/(app)/predictions/DriverComponent.tsx
+++ src/app/(app)/predictions/DriverComponent.tsx
@@ -11,7 +11,7 @@ export const DriverComponent: FC<DriverComponentProps> = ({ driver, className, .
   return (
     <div className={cn("flex gap-2 items-center", className)} {...props}>
       {driver.image && <img src={driver.image} alt={`No Picture`} className="h-10 w-10 text-xs sm:h-14 sm:w-14" />}
-      <div className="text-left mt-1">
+      <div className="text-left sm:mt-1">
         <div className="text-sm sm:text-lg/4">{driver.name}</div>
         <div className="text-xs sm:text-sm sm:mt-1 text-muted-foreground">{driver.team}</div>
       </div>
